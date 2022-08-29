f5c5488f89f8474c130a29cfd286e18b2a8ca7f1
MarianoIDC <mmunoz@estudiantec.cr>
2022-08-28 20:00:13 -0600

    update

--- Servidor/DocServer.c
+++ Servidor/DocServer.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h> //for socket APIs
+#include <time.h>
 #include <arpa/inet.h> 
 //#define SIZE 8192
 
@@ -14,10 +15,9 @@ void write_log(char *log, char *filename){
     if (NULL == log_file) {
         printf("file can't be opened \n");
     }
-    printf("Escribiendo en el Log\n");
+    printf("\n");
     printf("%s\n", log);
     fprintf(log_file, "%s\n", log); // write to file
-    printf("Se supone que ya se escribio\n");
 }
 
 int count_consonants(char *buffer) {
@@ -38,6 +38,12 @@ int count_consonants(char *buffer) {
 }
 
 void DocServer() {
+
+    int hours, minutes, seconds, day, month, year;
+ 
+    // `time_t` is an arithmetic time type
+    time_t now;
+
     // create server socket similar to what was done in
     // client program
     int servSockD = socket(AF_INET, SOCK_STREAM, 0);
@@ -48,11 +54,11 @@ void DocServer() {
     
     char log_filename[32];
     
-    char recv_filename[32];
+    char recv_dir[32];
     
     conf_fp = fopen("./server.conf", "r");
     
-    fscanf(conf_fp, "%d %s %s", &port, recv_filename, log_filename);
+    fscanf(conf_fp, "%d %s %s", &port, recv_dir, log_filename);
     
     fclose(conf_fp);
 
@@ -64,7 +70,14 @@ void DocServer() {
     if(servSockD < 0) {
         char *error = "Error in sockets!\n";
         perror(error);
-        // write_file(error);
+
+        now = time(&now);
+        char now1[100] = "Date>>>>>>";
+
+        strcat(now1, ctime(&now));
+        strcat(now1, error);
+
+        write_log(now1, log_filename);
         exit(1);
     }
 
@@ -73,22 +86,63 @@ void DocServer() {
     servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
     
     char *message_1 = "Server socket created!";
-    write_log(message_1, log_filename);
-    // bind socket to the specified IP and port
+
+    now = time(&now);
+    
+    char now2[100] = "Date>>>>>>";
+
+    strcat(now2, ctime(&now));
+    strcat(now2, message_1);
+
+    write_log(now2, log_filename);
+
+   // bind socket to the specified IP and port
     int e = bind(servSockD, (struct sockaddr*)&servAddr, sizeof(servAddr));
 
     if(e < 0) {
-        perror("Error in binding!\n");
+        //perror("Error in binding!\n");
+
+        time_t now_5;
+
+        now_5 = time(&now);
+
+        char now5[100] = "Date>>>>>>";
+        char message5[100] = "Error in binding!\n"; 
+        strcat(now5, ctime(&now_5));
+        strcat(now5, message5);
+
+        write_log(now5, log_filename);
+
         exit(1);
     }
+    time_t now_3;
 
-    printf("Binding successful!\n");
+    now_3 = time(&now);
+
+    char now3[100] = "Date>>>>>>";
+    char message2[100] = "Binding successful!\n"; 
+    strcat(now3, ctime(&now_3));
+    strcat(now3, message2);
+
+    write_log(now3, log_filename);
 
     // listen for connections
     e = listen(servSockD, 10);
 
     if (e == 0) {
-        printf("Listening...\n");
+        //printf("Listening...\n");
+
+        time_t now_4;
+
+        now_4 = time(&now);
+
+        char now4[100] = "Date>>>>>>";
+        char message4[100] = "Listening...\n"; 
+        strcat(now4, ctime(&now_4));
+        strcat(now4, message4);
+
+        write_log(now4, log_filename);
+
     } else {
         perror("Error in listening!\n");
         exit(1);
@@ -100,7 +154,17 @@ void DocServer() {
         // integer to hold client socket.
         addrSize = sizeof(newAddr);
         clientSocket = accept(servSockD, (struct sockaddr*)&newAddr, &addrSize);
-        printf("Socket aceptado\n");
+        time_t now_6;
+
+        now_6 = time(&now);
+
+        char now6[100] = "Date>>>>>>";
+        char message6[100] = "Socket aceptado\n"; 
+        strcat(now6, ctime(&now_6));
+        strcat(now6, message6);
+
+        write_log(now6, log_filename);
+        //printf("Socket aceptado\n");
 
         int buffer_size = 0;
         recv(clientSocket, &buffer_size, sizeof(buffer_size), 0);
@@ -115,9 +179,9 @@ void DocServer() {
         recv(clientSocket, serMsg, sizeof(serMsg), 0);
 
         printf("Buffer: %s\n", serMsg);
-        
+
         FILE* fp;
-        fp = fopen(recv_filename, "a");
+        fp = fopen(recv_dir, "a");
         if (fp == NULL) {
              perror("Error in creating file!\n");
              exit(1);
