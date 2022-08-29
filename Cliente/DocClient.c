e41834c72b9739b9bafe4de6b2bb5f7ec64991bc
Java-Sith <prieto.luisdaniel@gmail.com>
2022-08-28 23:49:21 -0600

    Pruebas finales

--- Cliente/DocClient.c
+++ Cliente/DocClient.c
@@ -1,34 +1,21 @@
-#include <netinet/in.h> //structure for storing address information
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/socket.h> //for socket APIs
+#include <sys/socket.h> //Librería que crea los sockets
 #include <string.h>
-#include <arpa/inet.h> 
-//#define SIZE 8192
-
-/*void send_file(FILE *file, int socket) {
-    char data[SIZE];
-    while (fgets(data, SIZE, file) != NULL) {
-        if (send(socket, data, sizeof(data), 0) == -1) {
-            perror("Error in sending data!\n");
-            exit(1);
-        }
-    }
-    // closing the file
-    bzero(data, SIZE);
-    printf("Archivo enviado\n");
-    fclose(file);
-}*/
+#include <arpa/inet.h> //Librería para las conexiones
 
 void DocClient(char* ip, int port) {
 
     printf("############################\n");
 
+    //Define la dirección del cliente
     struct sockaddr_in servAddr;
+    //Define el puntero al archivo
     FILE *fp;
     
     do{
+        //Crea el socket del cliente
         int sockD = socket(AF_INET, SOCK_STREAM, 0);
         
         if(sockD < 0) {
@@ -36,12 +23,14 @@ void DocClient(char* ip, int port) {
             exit(1);
         }
 
+        //Define los parámetros del socket
         servAddr.sin_family = AF_INET;
         servAddr.sin_port = port;
         servAddr.sin_addr.s_addr = inet_addr(ip);
 
         printf("Client socket created!\n");
-    
+
+        //Conecta el socket con sus parámetros
         int e = connect(sockD, (struct sockaddr*)&servAddr, sizeof(servAddr));
 
         if(e == -1) {
@@ -51,20 +40,21 @@ void DocClient(char* ip, int port) {
 
         printf("Connection successful!\n");
         
+        //Recibe la ruta del archivo del usuario
         char filename[20];
         char end[] = "end";
 
         printf("Ingrese la ruta del archivo:\n");
         scanf("%s", filename);
 
-        printf("Filename: %s\n", filename);
-
+        //Compara la ruta del archivo con el string end, si es igual entonces cierra el socket
         int comparacion = strcmp(filename, &end);
         
-        if(comparacion==0){
+        if(comparacion == 0){
             close(sockD);
             exit(1);
         } else {
+            //Abre el archivo de prueba
             fp = fopen(filename, "r");
 
             if (fp == NULL) {
@@ -72,15 +62,17 @@ void DocClient(char* ip, int port) {
             } else {
                 fseek(fp, 0L, SEEK_END);
   
-                // calculating the size of the file
+                //Calcula el tamaño del archivo
                 int file_size = ftell(fp);
 
                 file_size = file_size*2;
 
                 fseek(fp, 0L, 0);
 
+                //Envía el tamaño del archivo al servidor para ser aprovechado
                 send(sockD, &file_size, sizeof(file_size), 0);
 
+                //Buffer que recibe los datos del archivo
                 char data[file_size];
 
                 if (fgets(data, file_size, fp) != NULL)
@@ -88,12 +80,15 @@ void DocClient(char* ip, int port) {
                     puts(data);
                 }
                 
+                //Envía el buffer con los contenidos del archivo al servidor
                 send(sockD, data, sizeof(data), 0);
 
                 printf("Enviado!\n");
 
+                //Cantidad de consonantes enviadas por el cliente
                 int received_int = 0;
 
+                //Recibe la cantidad de consonantes del cliente
                 recv(sockD, &received_int, sizeof(received_int), 0);
 
                 printf("Message: %d\n", received_int);
--- Servidor/DocServer.c
+++ Servidor/DocServer.c
@@ -3,13 +3,12 @@
 // Mariano Munioz Masis - Luis Daniel Prieto Sibajo
 // Contador de Consonantes
 // 
-#include <netinet/in.h> //structure for storing address information
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h> //for socket APIs
+#include <sys/socket.h> //Librería que crea los sockets
 #include <time.h>
-#include <arpa/inet.h> 
+#include <arpa/inet.h>  //Librería para las conexiones
 
 // Aplicacion que escribe en el log de salida.
 
@@ -34,19 +33,22 @@ void write_log(char *log, char *filename){
     fprintf(log_file, "%s\n", now_t); // write to file
 }
 
-// Funcion que cuenta las consunantes
+// Funcion que cuenta las consonantes
 
 int count_consonants(char *buffer) {
     int consonants = 0;
     for (int i = 0; i < strlen(buffer); ++i) {
-        //printf("Buffer: %c\n", buffer[i]);
+        //Se evalúa el buffer con el valor ASCII de sus caracteres para que solo tome en cuenta alfabéticos
         if ((buffer[i] >= 65 && buffer[i] <= 90) || (buffer[i] >= 97 && buffer[i] <= 122)) {
+            //Busca si el caracter del buffer coincide con una vocal
             if (buffer[i] == 'a'|| buffer[i] =='e'|| buffer[i] =='i'|| buffer[i] == 'o' || buffer[i] == 'u' || buffer[i] == 'A' || buffer[i] == 'E' || buffer[i] == 'I' || buffer[i] == 'O' || buffer[i] == 'U') {
                 consonants += 0;
             } else {
+                //Si no es una vocal entonces suma las consonantes
                 consonants += 1;
             }
         } else {
+            //Si no es ninguno de los casos no se altera la variable de las consonantes
             consonants += 0;
         }
     }
@@ -58,24 +60,27 @@ int count_consonants(char *buffer) {
 void DocServer() {
 
 
-    // create server socket similar to what was done in
-    // client program
+    //Crea el socket del servidor
     int servSockD = socket(AF_INET, SOCK_STREAM, 0);
     int port;
+
+    //Abre el archivo de configuración con sus parámetros
     FILE *conf_fp;
     char log_filename[32];
     char recv_dir[32];
     
     conf_fp = fopen("./server.conf", "r");
     
+    //Con el archivo de configuración recibe el puerto, dirección de recibido y dirección de log
     fscanf(conf_fp, "%d %s %s", &port, recv_dir, log_filename);
     fclose(conf_fp);
 
-    // define server address
+    //Define la dirección del servidor y su tamaño
     struct sockaddr_in servAddr, newAddr;
 
     socklen_t addrSize;
 
+    //Define si no pudo crear el socket
     if(servSockD < 0) {
         char *error = "Error in sockets!\n";
         perror(error);
@@ -84,14 +89,16 @@ void DocServer() {
         exit(1);
     }
 
+    //Define los parámetros del socket server
     servAddr.sin_family = AF_INET;
     servAddr.sin_port = port;
     servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
     
+    //Empieza a escribir en el log
     char *message_soc_created = "Server socket created!\n";
     write_log(message_soc_created, log_filename);
 
-   // bind socket to the specified IP and port
+    //Conecta el socket con los parámetros especificados
     int e = bind(servSockD, (struct sockaddr*)&servAddr, sizeof(servAddr));
 
     if(e < 0) {
@@ -101,10 +108,11 @@ void DocServer() {
 
         exit(1);
     }
+
     char message_dind_succ[100] = "Binding successful!\n"; 
     write_log(message_dind_succ, log_filename);
 
-    // listen for connections
+    //Empieza a escuchar las conexiones
     e = listen(servSockD, 10);
 
     if (e == 0) {
@@ -118,6 +126,7 @@ void DocServer() {
         write_log(message_listen_fail, log_filename);
         exit(1);
     }
+    //Define el socket que va a recibir el cliente
     int clientSocket;
 
     do {
@@ -126,28 +135,32 @@ void DocServer() {
         // linea que ejecuta el comando para las llamadas al sistema
         system("strace -n -i -ttt -o systemcalls.txt ./DocServer");
 	
-        // integer to hold client socket.
+        //Tamaño de la dirección del servidor
         addrSize = sizeof(newAddr);
+        //Acepta el socket del cliente
         clientSocket = accept(servSockD, (struct sockaddr*)&newAddr, &addrSize);
         
         char *message_sock_accept = "Socket aceptado\n"; 
         write_log(message_sock_accept, log_filename);
         printf("Socket aceptado\n");
 
+        //Define el tamaño del buffer enviado por el cliente
         int buffer_size = 0;
+        //Recibe el tamaño del buffer enviado por el cliente
         recv(clientSocket, &buffer_size, sizeof(buffer_size), 0);
 
         printf("Size: %d\n", buffer_size);
 
         
-	// string store data to send to client
+	    //Buffer que recibe el contenido del archivo del servidor
     	char serMsg[buffer_size];
     	
-        // send messages to client socket
+        //Recibe el contenido del archivo del servidor
         recv(clientSocket, serMsg, sizeof(serMsg), 0);
 
         printf("Buffer: %s\n", serMsg);
 
+        //Declara el archivo que va a escribir lo que se recibió del buffer
         FILE* fp;
         fp = fopen(recv_dir, "a");
         if (fp == NULL) {
@@ -158,12 +171,14 @@ void DocServer() {
         }
         fprintf(fp, "%s\n", serMsg);
         
+        //Invoca la función para contar las consonantes del buffer
         int consonants = count_consonants(serMsg);
         printf("Consonants: %d\n", consonants);
 
         char *message_send = "Message Send!\n"; 
         write_log(message_send, log_filename);
 
+        //Envía la cantidad de consonantes que entregó la función al cliente
         send(clientSocket, &consonants, sizeof(consonants), 0);
 
         bzero(serMsg, buffer_size);
